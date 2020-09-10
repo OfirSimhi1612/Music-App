ec159af8eae00b2adb3fc056800b0165539aa361
OfirSimhi1612 <67858057+OfirSimhi1612@users.noreply.github.com>
2020-09-10 19:02:53 +0300

    POST methods  works
    -song
    -playlist
    -artist
    -album

--- server/app.js
+++ server/app.js
@@ -21,15 +21,152 @@ let mysqlCon = mysql.createConnection({
   });
 
 mysqlCon.connect(err => {
-    if (err) console.log(err);
-    console.log("Connected!");
+    if(err){
+        console.log('error conecting to server: ' + err.message);
+    } else {
+        console.log("Connected to SQL server!");
+    }
 });
 
-// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'nir123';
-// flush privileges; - need to do this to give premission on sql workbanch
+// Each time user play song it save it as interactions with the user, song, is_liked, play_count, created_at
+// a GET request to /top_songs/ returns a list of top 20 songs
+
+// a GET request to /top_artists/ returns a list of top 20 artists
+
+// a GET request to /top_albums/ returns a list of top 20 albums
+
+// a GET request to /top_playlist/ returns a list of top 20 playlist
+
+// a GET request to /song/123 returns the details of song 123
+
+// a GET request to /artist/123 returns the artist 123
+
+// a GET request to /album/123 returns the album 123
+
+// a GET request to /playlist/123 returns the playlist 123
+
+// a POST request to /song add to songs
+
+// a POST request to /album add to album
+
+// a POST request to /playlist add to playlist
+
+// a POST request to /artist add to artist
+
+// a PUT request to /song/123 update the details of song 123
+
+// a PUT request to /artist/123 update the artist 123
+
+// a PUT request to /album/123 update the album 123
+
+// a PUT request to /playlist/123 update the playlist 123
+
+// a DELETE request to /song/123 delete the details of song 123
+
+// a DELETE request to /artist/123 delete the artist 123
+
+// a DELETE request to /album/123 delete the album 123
+
+// a DELETE request to /playlist/123 delete the playlist 123
 
 
-app.get('/songs', (req, res) => {
+function newDateToSQL(){
+    return new Date().toISOString().slice(0, -5).replace('T', ' ')
+}
+
+
+
+app.post('/playlists', (req, res) => { 
+    const data = req.body;
+
+    mysqlCon.query(`INSERT INTO playlists (name, cover_img, created_at, uploaded_at, genre)
+                VALUES ('${data.name}', '${data.cover_img}', '${data.created_at}', '${newDateToSQL()}','${data.genre}')`,
+                (error, results, fields) => {
+                    if(error){
+                        res.status(400).send(error.message);
+                        console.log(error)
+                    } else {
+                        res.json(data)
+                    }
+                })
+})
+
+
+app.post('/artists', (req, res) => { //working
+    const data = req.body;
+    
+    mysqlCon.query(`INSERT INTO artists (first_name, last_name, birth_date, cover_img, uploaded_at, likes)
+                VALUES ('${data.first_name}', '${data.last_name}', '${data.birth_date}',${data.cover_img}, '${newDateToSQL()}', '${data.likes}')`,
+                (error, results, fields) => {
+                    if(error){
+                       res.status(400).send(error.message);
+                    } else {
+                        res.json(data)
+                    }
+                })
+})
+
+
+app.post('/albums', (req, res) => { // working
+    const data = req.body;
+    if(data.artist_id){
+       return  mysqlCon.query(`SELECT artist_id FROM artists WHERE artist_id = ${data.artist_id}`, (err, results) => {
+            if(results.length === 0){
+               res.status(404).send('invalid artist id : no artist with such id found');
+            }
+            if(err){
+                res.send(err.message)
+            } 
+        })
+    }
+    mysqlCon.query(`INSERT INTO albums (name,  artist_id, published_at, uploaded_at, likes)
+                VALUES ('${data.name}', '${data.artist_id}', '${data.published_at}', '${newDateToSQL()}', '${data.likes}')`,
+                (error, results, fields) => {
+                    if(error){
+                       res.status(400).send(error.message);
+                    } else {
+                        res.json(data)
+                    }
+                })
+})
+
+
+app.post('/songs', (req, res) => { // working
+    const data = req.body;
+    if(data.artist_id){
+        return mysqlCon.query(`SELECT artist_id FROM artists WHERE artist_id = ${data.artist_id}`, (err, results) => {
+            if(results.length === 0){
+                res.status(404).send('invalid artist id : no artist with such id found');
+             }
+             if(err){
+                 res.send(err.message)
+             } 
+        })
+    }
+    if(data.album_id){
+         return mysqlCon.query(`SELECT album_id FROM albums WHERE album_id = ${data.album_id}`, (err, results) => {
+            if(results.length === 0){
+                res.status(404).send('invalid album id : no album with such id found');
+             }
+             if(err){
+                 res.send(err.message)
+             } 
+        })
+    }
+    mysqlCon.query(`INSERT INTO songs (title, artist_id, album_id, lyrics, length, created_at, uploaded_at, youtube_link, track_number, likes)
+                VALUES ('${data.title}', '${data.artist_id}', '${data.album_id}', '${data.lyrics}', '${data.length}', '${data.created_at}', '${newDateToSQL()}', '${data.youtube_link}', '${data.track_number}', '${data.likes}')`,
+                (error, results, fields) => {
+                    if(error){
+                        res.status(400).send(error.message);
+                        console.log(error)
+                    } else {
+                        res.json(data)
+                    }
+                })
+})
+
+
+app.get('/songs', (req, res) => { //working
     mysqlCon.query('SELECT * FROM songs', (error, results, fields) => {
         if (error) {
             res.send(err.message);
@@ -38,13 +175,17 @@ app.get('/songs', (req, res) => {
       });
 });
 
-app.get('/song/:id', async (req, res) =>{
-    try{
-        const product = await Product.findById(req.params.id);
-        res.send(product);
-    } catch(err){
-        res.status(400).send(err.message);
-    }
+
+
+app.get('/songs/:id', async (req, res) =>{ //working
+    mysqlCon.query(`SELECT * FROM songs WHERE song_id = ${req.params.id}` ,(error, results, fields) => {
+        if (error) {
+            res.send(err.message);
+            throw error;
+        };
+        res.send(results);
+      });
 });
 
-app.listen(3000)
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+port = 8080;
+
+app.listen(port, console.log(`listening on port ${port}`));
