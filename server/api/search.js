6022809a6966bd983badf2b8ca7176124d8a4758
ofir <ofirsimhi1612@gmail.com>
2020-11-12 10:55:50 +0200

    client using new search end point

--- server/api/search.js
+++ server/api/search.js
@@ -1,37 +1,27 @@
 const { Router } = require('express');
-const { postSearchDoc, deleteSearchDoc, updateSearchDoc, getDocIdBySQLId } = require('../elastic_search')
 const Songs = require('../elastic_search/songs');
 const Artists = require('../elastic_search/artists');
 const Albums = require('../elastic_search/albums');
 const Playlists = require('../elastic_search/playlists');
-const { updateAllFromDB } = require('../elastic_search');
 
 const router = Router();
 
-router.get('/all', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const searchValue = req.query.search;
 
         const searchData = await Promise.all([
             Songs.search(searchValue || ''),
-            Artists.search(searchValue || ''),
             Albums.search(searchValue || ''),
+            Artists.search(searchValue || ''),
             Playlists.search(searchValue || '')
         ])
 
-        res.json(searchData.map(result => result.hits.hits))
+        res.json(searchData.map(result => result.hits.hits.map(e => e._source)))
     } catch (error) {
         console.log(error);
     }
 })
 
-router.get('/test', async (req, res) => {
-    try{
-        await updateAllFromDB()
-        res.send('finish')
-    } catch(error){
-        console.log(error)
-    }
-})
 
 module.exports =  router
