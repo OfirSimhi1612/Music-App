a73ee5e06677b031bfb7419c090a2f81a9f8351c
ofir <ofirsimhi1612@gmail.com>
2020-11-13 14:51:27 +0200

    elastic search updete function
    deleting doc if doesnt exist in db

--- server/elastic_search/helpers/compare.js
+++ server/elastic_search/helpers/compare.js
@@ -50,11 +50,25 @@ async function getDocIdBySQLId(index, id){
     }
 }
 
+async function removeSearchDoc(index, id){
+    try{
+        await client.delete({
+            index,
+            id
+        })
+        return
+    } catch(error){
+        throw error
+    }
+    
+}
+
 async function indexCompare(index ,baseList, compareList){
     try{
         let new_counter = 0;
         let mod_counter = 0;
         let same_counter = 0;
+        let del_counter = 0;
         for(let i = 0; i < baseList.length; i++){
             const matchingElement = compareList.find((match) =>{
                 return match.id === baseList[i].id
@@ -66,18 +80,31 @@ async function indexCompare(index ,baseList, compareList){
                 const matching = JSON.stringify(matchingElement) === JSON.stringify(baseList[i])
                 if(!matching) {
                     mod_counter ++
-                    const DocId = await getDocIdBySQLId(index, matchingElement.id, baseList[i])
+                    const DocId = await getDocIdBySQLId(index, matchingElement.id)
                     await updateSearchDoc(index, DocId, baseList[i])
                 } else {
                     same_counter ++
                 }
             }
         }
-        console.log('ELASTIC SEARCH UPDATE:')
-        console.log('added ' + new_counter + ' ' + index + 's')
-        console.log('modifyed ' + mod_counter + ' ' + index + 's')
-        console.log(same_counter + ' ' + index + 's up to date')
-        console.log('_____________________________________')
+        if(compareList.length > baseList.length){
+            for(let i = 0; i < compareList.length; i++){
+                const matchingElement = baseList.find((match) =>{
+                    return match.id === compareList[i].id
+                })
+                if(!matchingElement){
+                    const DocId = await getDocIdBySQLId(index, compareList[i].id)
+                    await removeSearchDoc(index, DocId);
+                    del_counter ++;
+                }
+            }
+        }
+        console.log(`ELASTIC SEARCH UPDATE: (${index}s)`);
+        console.log('added ' + new_counter);
+        console.log('modifyed ' + mod_counter);
+        console.log('deleted ' + del_counter);
+        console.log(same_counter + ' up to date');
+        console.log('_____________________________________');
     } catch (error){
         console.log(error)
         throw error
