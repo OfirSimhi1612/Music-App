b5025fbb176df4d3ee9859348c4ed429d74bf500
ofir <ofirsimhi1612@gmail.com>
2020-11-15 22:02:13 +0200

    docker file to client and server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, console.log(`listenning on port ${port}
-env:${process.env.NODE_ENV}
-`));
+env:${process.env.NODE_ENV}`));
