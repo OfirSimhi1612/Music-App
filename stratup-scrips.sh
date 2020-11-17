a54ab69ffc25480e23a20f7c4a4abf1b3fbc00da
ofir <ofirsimhi1612@gmail.com>
2020-11-17 20:06:48 +0200

    startup script and vm comands

---
+++ stratup-scrips.sh
@@ -0,0 +1,57 @@
+set -v
+
+
+# Talk to the metadata server to get the project id
+PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+REPOSITORY="music-app"
+
+# Install logging monitor. The monitor will automatically pick up logs sent to
+# syslog.
+curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
+service google-fluentd restart &
+
+# Install dependencies from apt
+apt-get update
+apt-get install -yq ca-certificates git build-essential supervisor
+
+
+# Installing Docker runtime
+sudo apt-get update
+
+yes | sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+yse | yes | sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker -v
+sudo docker-compose -v
+
+# Get the application source code from the Google Cloud Repository.
+# git requires $HOME and it's not set during the startup script.
+export HOME=/root
+git config --global credential.helper gcloud.sh
+git clone https://source.developers.google.com/p/${PROJECTID}/r/${REPOSITORY} /opt/app/music-app
+
+# Install app dependencies
+cd /opt/app/music-app
+sudo docker-compose up
+EOF
+
+supervisorctl reread
+supervisorctl update
+
+# Application should now be running under supervisor
